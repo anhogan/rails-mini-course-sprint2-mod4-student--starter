b853c87385349e57cdda8db9790756d0cbbcb084
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-05-14 11:33:37 -0400

    Write request spec for creating employee

--- spec/requests/create_employees_spec.rb
+++ spec/requests/create_employees_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Create Employees", type: :request do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Create an Employee Request" do
+    context "POST /employees with Valid Request" do
+      it "Creates and Returns the Employee" do
+        valid_params = { first_name: "New", last_name: "Employee", rewards_balance: nil }
+
+        post "/employees", params: valid_params
+        response_body = JSON.parse(response.body).deep_symbolize_keys
+
+        expect(response).status.to be :created
+        expect(response_body).to include({
+          first_name: "New",
+          last_name: "Employee",
+          rewards_balance: nil
+        })
+      end
+    end
+
+    context "POST /employees with Invalid Request" do
+      it "Fails to Create and Returns Error" do
+        invalid_params = { first_name: nil, last_name: nil, rewards_balance: nil }
+
+        post "/employees", params: invalid_params
+
+        expect(response).status.to be :unprocessable_entity
+        expect(errors.count).to be > 0
+      end
+    end
+  end
 end
