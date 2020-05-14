6008db96aebea15d5392eca8a522ffbbcd94659f
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-05-14 12:30:57 -0400

    fix request tests

--- spec/requests/create_employees_spec.rb
+++ spec/requests/create_employees_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe "Create Employees", type: :request do
   describe "Create an Employee Request" do
     context "POST /employees with Valid Request" do
       it "Creates and Returns the Employee" do
-        valid_params = { first_name: "New", last_name: "Employee" }
+        valid_params = { employee: {first_name: "New", last_name: "Employee", rewards_balance: nil} }
+        puts(params: valid_params)
 
         post "/employees", params: valid_params
         response_body = JSON.parse(response.body).deep_symbolize_keys
 
-        expect(response).status.to be :created
+        expect(response).to have_http_status(201)
         expect(response_body).to include({
           first_name: "New",
           last_name: "Employee",
@@ -20,12 +21,12 @@ RSpec.describe "Create Employees", type: :request do
 
     context "POST /employees with Invalid Request" do
       it "Fails to Create and Returns Error" do
-        invalid_params = { first_name: nil, last_name: nil }
+        invalid_params = { employee: {first_name: nil, last_name: nil, rewards_balance: nil} }
+        puts(params: invalid_params)
 
         post "/employees", params: invalid_params
 
-        expect(response).status.to be :unprocessable_entity
-        expect(errors.count).to be > 0
+        expect(response).to have_http_status(422)
       end
     end
   end
