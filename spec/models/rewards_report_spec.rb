0d66050824b2539bb5908df0cdf947bb9967b911
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-05-14 11:46:00 -0400

    Write isolated tests with doubles and spies

--- spec/models/rewards_report_spec.rb
+++ spec/models/rewards_report_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe RewardsReport do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Rewards Report Notify" do
+    let(:rewards) [
+      { double("Reward", cost: 30, inventory: 5, deactivated_at: nil, name: "First", pucharse_count: 30) },
+      { double("Reward", cost: 20, inventory: 5, deactivated_at: nil, name: "Second", pucharse_count: 70) },
+      { double("Reward", cost: 10, inventory: 5, deactivated_at: nil, name: "Third", pucharse_count: 100) }
+    ]
+
+    it "Should Send Email with Purchase Report" do
+      total = rewards.total_purchases
+      
+      expect(NotificationService).to receive(total).with(200)
+    end
+  end
 end
